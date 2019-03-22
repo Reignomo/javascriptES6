005ffed15eec6ff14c515833a7e74f809cf39d31
Evandro <eignacio403@gmail.com>
2019-03-22 11:36:14 -0300

    Exercicio2 finalizado

---
+++ Desafios/Modulo1/Exercicio1.js
@@ -0,0 +1,29 @@
+
+class Usuario {
+
+    constructor(email,senha){
+        this.email = email;
+        this.senha = senha;
+    }
+
+   // isAdmin = () => { return this.admin ;}
+   isAdmin = () =>  {
+      if(this.admin === true){
+        return this.admin
+      }
+      return false;
+   }
+}
+class Admin extends Usuario {
+
+    constructor(email,senha){
+        super(email,senha);
+        this.admin = true;
+    }
+    
+}
+
+var user = new Usuario('evandroig@gmail.com','123');
+var admin = new Admin('gab@objetivo.com.br','olo');
+console.log(`user is admin: ${user.isAdmin()}`);
+console.log(`admin is admin: ${admin.isAdmin()}`);
